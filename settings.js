19c5c68af86e5ceba3ab1c8e0a399c6bc1857338
Subhan66 <95126258+Subhan66@users.noreply.github.com>
2023-04-15 18:24:27 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -55,21 +55,21 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.ownername = "VIRUSBABA //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
+global.owner = ['923166771794']
+global.ownertag = '923166771794' //ur tag number
 global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.linkz = "https://www.virusbaba.com" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.virusbaba.com" //ur website to be displayed
+global.botscript = 'https://github.com //script link
+global.reactmoji = "❤️" //ur menu react emoji
+global.themeemoji = "💀" //ur theme emoji
+global.packname = "Chems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
 global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
 
 //Bot theme media
